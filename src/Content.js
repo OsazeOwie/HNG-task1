b1426a64387b6f1450dbbfb3de568bad588b9a6a
oz <osazeowie0@gmail.com>
2022-11-01 17:26:27 +0100

    add subtext to links

--- src/Content.js
+++ src/Content.js
@@ -22,12 +22,12 @@ const Content = () => {
             </div>
           
             <div className="link-section">
-                <a href="https://twitter.com/@OZ_SkyDaddy" className="link-style">Twitter Link</a>
-                <a href="https://training.zuri.team/" className="link-style"  id='btn__zuri'>Zuri Team</a>
-                <a href=" http://books.zuri.team" className="link-style"  id='books'>Zuri Books</a>
-                <a href="https://books.zuri.team/python-for-beginners?ref_id=Osaze Owie" className="link-style"  id='book__python'>Python Books</a>
-                <a href="https://background.zuri.team" className="link-style" id='pitch'>Background Check for Coders</a>
-                <a href="https://books.zuri.team/design-rules" className="link-style" id='book__design'>Design Books</a>
+                <a href="https://twitter.com/@OZ_SkyDaddy"  className="link-style">Twitter Link</a>
+                <a href="https://training.zuri.team/"  className="link-style"  id='btn__zuri'>Zuri Team</a>
+                <a href=" http://books.zuri.team" title='Get design and coding books here' className="link-style"  id='books'>Zuri Books</a>
+                <a href="https://books.zuri.team/python-for-beginners?ref_id=Osaze Owie" title='Buy my python books here' className="link-style"  id='book__python'>Python Books</a>
+                <a href="https://background.zuri.team" title='Get thorough background checks of coders here' className="link-style" id='pitch'>Background Check for Coders</a>
+                <a href="https://books.zuri.team/design-rules" title='Get design books for free' className="link-style" id='book__design'>Design Books</a>
             </div>
         </main>
      );
